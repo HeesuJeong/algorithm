3a22d0528972f53d64272dc300305930566f5cdd
HeesuJeong <jungjera2@gmail.com>
2019-10-16 15:36:28 +0900

    1016

---
+++ algorithm/14주차/텀프로젝트.java
@@ -0,0 +1,25 @@
+package 수요일;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 텀프로젝트 {
+
+	static int[] parents;
+	static int[] ranks;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int TC=Integer.parseInt(br.readLine());
+		for (int tc = 0; tc < TC; tc++) {
+			int n=Integer.parseInt(br.readLine());
+			parents=new int[n+1];// 1부터 
+			for (int i = 0; i <n; i++) {
+				
+			}
+		}//tc
+	}
+
+}
